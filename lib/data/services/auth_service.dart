bba76c65c86c3a750676fcc4fc47ba91cd7303cd
Lázaro Marques <lazaro.sobral@facape.br>
2026-03-09 21:38:17 -0300

    refactor: add session service

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -1,25 +1,37 @@
-import 'package:glorious_tcc/data/repositories/auth_repository.dart';
+import 'package:glorious_tcc/data/repositories/user_repository.dart';
+import 'package:glorious_tcc/data/services/password_hasher.dart';
+import 'package:glorious_tcc/data/services/session_service.dart';
 
 class AuthService {
-  final AuthRepository repository;
+  final UserRepository repository;
+  final SessionService sessionService;
 
-  AuthService(this.repository);
+  AuthService(this.repository, this.sessionService);
 
   Future<bool> login({
     required String username,
     required String password,
   }) async {
-    return await repository.login(
-      username: username,
-      password: password,
+    final user = await repository.findByEmail(
+      username,
     );
+
+    if (user == null) return false;
+
+    if (!PasswordHasher.verify(password, user.password)) return false;
+
+    final bool isSave = await sessionService.saveSession(user: user);
+
+    if (!isSave) return false;
+
+    return true;
   }
 
   Future<bool> isLogged() async {
-    return await repository.isLogged();
+    return await sessionService.isLogged();
   }
 
   Future<bool> logout() async {
-    return await repository.logout();
+    return await sessionService.clear();
   }
 }
