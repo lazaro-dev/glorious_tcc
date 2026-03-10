897f3cf841ce70143414fc13918adf48a63f482e
Lázaro Marques <lazaro.sobral@facape.br>
2026-03-09 21:37:30 -0300

    feat: new methods

--- lib/data/services/session_service.dart
+++ lib/data/services/session_service.dart
@@ -1,16 +1,26 @@
+import 'dart:convert';
+
+import 'package:glorious_tcc/data/models/user_dto.dart';
+import 'package:glorious_tcc/data/services/local_storage_service.dart';
+import 'package:glorious_tcc/domain/entities/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SessionService {
   static const _accountIdKey = 'account_id';
   static const _userIdKey = 'user_id';
+  static const _userKey = 'user';
 
-  Future<void> saveSession({
-    required int userId,
-    required int accountId,
+  Future<bool> saveSession({
+    required User user,
   }) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_userIdKey, userId);
-    await prefs.setInt(_accountIdKey, accountId);
+    await prefs.setInt(_userIdKey, user.id!);
+    await prefs.setInt(_accountIdKey, user.accountId!);
+
+    final bool result = await LocalStorageService.instance
+        .setString(_userKey, jsonEncode(user.toJson()));
+
+    return result;
   }
 
   Future<int?> get accountId async {
@@ -23,8 +33,30 @@ class SessionService {
     return prefs.getInt(_userIdKey);
   }
 
-  Future<void> clear() async {
+  Future<User?> get user async {
+    final String? strUser =
+        await LocalStorageService.instance.getString(_userKey);
+
+    if (strUser == null) return null;
+
+    final jsonUser = json.decode(strUser);
+
+    final user = UserDTO.fromMap(jsonUser);
+
+    return user.toDomain();
+  }
+
+  Future<bool> isLogged() async {
+    final String? strUser =
+        await LocalStorageService.instance.getString(_userKey);
+
+    return (strUser != null) ? true : false;
+  }
+
+  Future<bool> clear() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.clear();
+    final bool result = await prefs.clear();
+
+    return result;
   }
 }
